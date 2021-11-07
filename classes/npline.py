need to figure out rotation is not correct, fixed angle threshold, utilized arctan2

classes/npline.py
@@ -2,11 +2,11 @@ import numpy as np
 from classes.point import Point
 from classes.points import Points, init_args
 import cv2
-import math
 
 
-class Line:
-    def __init__(self, points: np.ndarray = None, start_point: Point = None, end_point: Point = Point):
+class Line(np.ndarray):
+    @init_args
+    def __init__(cls, list_of_points):
         self.points = Points(np.zeros((2, 2)))
         self.center = Point([0, 0])
         self.length = 0
@@ -43,7 +43,7 @@ class Line:
         # Calc line attributes after either defining points or start and end point
         self.calc_attributes()
 
-    def __repr__(self):
+    def __str__(self):
         return 'Line: (Start Point {0}, Center Point {1}, End Point {2})'\
             .format(self.points[0], self.center, self.points[1])
 
@@ -89,11 +89,9 @@ class Line:
         return rnl * (scale / norm) + self.center, rnr * (scale / norm) + self.center
 
     def get_angle(self):
-
-        dx = self.points[0][0] - self.points[1][0]
-        dy = self.points[0][1] - self.points[1][1]
-        return math.atan2(dy, dx)
-        # return math.atan((dy/dx))
+        dx = self.points[1][0] - self.points[0][0]
+        dy = self.points[1][1] - self.points[0][1]
+        return np.arctan(dy/dx)
 
     @property
     def start(self):

editor_GUI/step_sequencer/OLD/test.py
@@ -1,3 +1,32 @@
-lst = [1,2,3]
+import math
 
-print(lst[None])
+import numpy as np
+from matplotlib import pyplot as plt
+from classes.rectangle import Rectangle
+from classes.line import Line
+from math import isclose
+rect = np.array([[340.1042893,  106.41947853], [665.73452843,  47.0397904 ], [725.11421656, 372.67002953], [399.48397743, 432.04971766]])
+rect = np.array([[443.89828813, -31.13778519], [792.1126566,   56.19922638], [710.12777388, 383.07455277], [361.91340541, 295.7375412 ]])
+rect = np.array([[469.55772492, -16.73671388], [794.05327125,  74.21184525], [709.85163788, 374.63502168], [385.35609154, 283.68646255]])
+def is_orthogonal(a, b, c):
+    return isclose((b[0] - a[0]) * (b[0] - c[0]) + (b[1] - a[1]) * (b[1] - c[1]), 0, abs_tol=1e-3)
+    # return (b[0] - a[0]) * (b[0] - c[0]) + (b[1] - a[1]) * (b[1] - c[1])
+
+print(is_orthogonal(rect[3], rect[0], rect[1]))
+print(is_orthogonal(rect[0], rect[1], rect[2]))
+print(is_orthogonal(rect[1], rect[2], rect[3]))
+
+x, y = rect.T
+# plt.plot(x, y)
+# plt.show()
+
+# Test angle calculation, returining -180, should be zero
+def get_angle(self):
+    dx = self.points[0][0] - self.points[1][0]
+    dy = self.points[0][1] - self.points[1][1]
+
+    return math.atan((dy/dx))
+
+points = Line(np.array([[74, 0], [74, 31]]))
+print('get_angle: ', np.rad2deg(get_angle(points)))
+

editor_GUI/step_sequencer/find_line_test.py
@@ -81,12 +81,12 @@ if __name__ == "__main__":
         if roi_rect.return_flag:
             break
 
-    cv2.destroyWindow(wName)
+    # cv2.destroyWindow(wName)
     time.sleep(0.5)
     start_time = time.time()
-    img2 = crop_rotated_rectangle(image, roi_rect.rectangle)
-    # cv2.imshow('rotated rect', img2)
+    img2, cropped_ROI = crop_rotated_rectangle(image, roi_rect.rectangle)
     cv2.namedWindow('output')
+    cv2.imshow('rotated rect', img2)
 
     # convert to grayscale and blur
     gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
@@ -119,9 +119,14 @@ if __name__ == "__main__":
     # plot all lines in red
     for i, line in enumerate(np_lines):
         cv_line = line.cv_format()
+        print(f'{i=}', f'{line=}')
         cv2.line(line_image, cv_line[0], cv_line[1], (0, 0, 255), 1)
         cv2.putText(line_image, 'Line: {}'.format(i), line.start.cv_format(), cv2.FONT_HERSHEY_SIMPLEX, .4, (0, 0, 255),
                     1, cv2.LINE_AA)
+
+    # Draw the lines on the  image
+    lines_edges = cv2.addWeighted(img2, 0.5, line_image, 1, 0)
+    cv2.imshow('output', lines_edges)
     del_index = []
     # TODO Functionize all line threshold tools, allow for plotting or not and enable debugging or not
     if len(np_lines) > 0:
@@ -133,14 +138,13 @@ if __name__ == "__main__":
 
         # delete any lines that are out of range
         for i, line in enumerate(np_lines):
-            line_angle = np.rad2deg((line.get_angle())) + 90     # degrees
-
-            if line_angle < - angle_threshold or line_angle > angle_threshold:
-                # print('delete index {}'.format(i), line_angle)
-                del_index.append(i)
-            else:
+            line_angle = np.rad2deg((line.get_angle())) - 90     # degrees
+            if -angle_threshold < line_angle < angle_threshold:
+                print('keep index {}'.format(i), line_angle)
                 pass
-                # print('keep index {}'.format(i), line_angle)
+            else:
+                print('delete index {}'.format(i), line_angle)
+                del_index.append(i)
         # create new array of in range values
         filtered_np_lines = [x for i, x in enumerate(np_lines) if i not in del_index]
         np_lines = filtered_np_lines[:]
@@ -150,7 +154,7 @@ if __name__ == "__main__":
     # ###############
     # Edge Polarity
     # ###############
-    edge_setting = EdgeSettings.blackToWhite
+    edge_setting = EdgeSettings.whiteToBlack
 
     # if edge polarity is set to either, skip all polarity checks
     if edge_setting.value != 0:
@@ -187,7 +191,6 @@ if __name__ == "__main__":
             cv2.line(line_image, start, end, (255, 0, 255), 1)
         # create new array of in range values
         filtered_np_lines = [x for i, x in enumerate(np_lines) if i not in del_index]
-        print(len(filtered_np_lines))
         np_lines = filtered_np_lines[:]
 
     if len(np_lines) > 0:
@@ -214,25 +217,43 @@ if __name__ == "__main__":
             search_idx = 0
             for i, line in enumerate(np_lines):
                 if line.center.x > value or i == 0:
-                    value = line.center.x
+
                     search_idx = i
             line_to_plot = np_lines[search_idx]
+            start, end = line_to_plot.cv_format()
+        cv2.line(line_image, start, end, (0, 255, 0), 1)
 
+        rotated = Points.rotate(line_to_plot.points, roi_rect.rectangle.attributes.rotation, cropped_ROI.attributes.center)
+        translate = Points.translate(rotated, cropped_ROI.tl)
+        # center_offset = (crop_center - line_to_plot.center)
+        # translate_to_center = Points.translate(line_to_plot.points, center_offset)
+        # rotated = Points.rotate(translate_to_center, roi_rect.rectangle.attributes.rotation, crop_center)
+        # translate_to_center = Points.translate(rotated, -center_offset)
+        # translate = Points.translate(translate_to_center, crop_center.tl)
+
+        # translate = line_to_plot.points.translate(line_to_plot.points, roi_rect.rectangle.tl)
+        # rotated = Points.rotate(translate, -roi_rect.rectangle.attributes.rotation, crop_center)
+        translate = translate.astype(int)
+        translate_start = tuple(translate[0])
+        translate_end = tuple(translate[1])
         # plot selected line
-        cv_line = line_to_plot.cv_format()
-        cv2.line(line_image, cv_line[0], cv_line[1], (0, 255, 0), 1)
-        cv2.putText(line_image, 'Line: {}'.format(i), line_to_plot.start.cv_format(), cv2.FONT_HERSHEY_SIMPLEX, 0.4,
-                    (0, 255, 0), 1, cv2.LINE_AA)
+        # cv_line = line_to_plot.cv_format()
+        cv2.line(image, translate_start, translate_end, (0, 255, 0), 3)
+        # cv2.putText(line_image, 'Line: {}'.format(i), line_to_plot.start.cv_format(), cv2.FONT_HERSHEY_SIMPLEX, 0.4,
+        #             (0, 255, 0), 1, cv2.LINE_AA)
+        cv2.imshow(wName, image)
 
-    # Draw the lines on the  image
-    lines_edges = cv2.addWeighted(img2, 0.5, line_image, 1, 0)
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print(elapsed_time)
-    cv2.imshow('output', lines_edges)
+        # Draw the lines on the  image
+        lines_edges = cv2.addWeighted(img2, 0.5, line_image, 1, 0)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f'{elapsed_time=}')
+        cv2.imshow('output', lines_edges)
 
-    while True:
-        key = cv2.waitKey(1) & 0xFF
-        if key == 27 or key == 13:
-            break
-    cv2.destroyWindow('output')
+        while True:
+            key = cv2.waitKey(1) & 0xFF
+            if key == 27 or key == 13:
+                break
+        cv2.destroyWindow('output')
+    else:
+        print('No Lines Found, exiting ...')

editor_GUI/step_sequencer/load_image_from_file.py
@@ -3,7 +3,8 @@ import cv2
 import numpy as np
 import os
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QGridLayout, QTextEdit, QFileDialog, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QGridLayout, QTextEdit, QFileDialog, QVBoxLayout, QComboBox,\
+    QFormLayout, QGroupBox
 from PyQt5.QtCore import pyqtSignal
 from PyQt5 import QtCore
 
@@ -29,7 +30,6 @@ class TextEdit(QTextEdit):
         file_name = file_browser.getOpenFileName(self, 'Open Image File', r"<Default dir>", file_types)
         return file_name[0]
 
-
     def mouseReleaseEvent(self, event):
         dialog_filepath = self.file_browser()
         if dialog_filepath != '':
@@ -48,7 +48,6 @@ class LoadImageFromFile(Step):
     def __init__(self, json=None):
         super().__init__(json)
         # maybe not needed now
-        self.image = None
         self.status = 0
 
         # required parameters
@@ -60,14 +59,15 @@ class LoadImageFromFile(Step):
 
     def execute(self, commands=None, counter=None):
         try:
-            self.image = cv2.imread(self.filepath)
+            self.__class__.images[self.output_image_index] = cv2.imread(self.filepath)
+            self.__class__.display_image = self.images[self.output_image_index]
             self.status = RAN_SUCCESS
         except (FileExistsError, FileNotFoundError):
             self.status = RAN_FAILED
 
     def print(self):
-        if self.image:
-            h, w, bpp = np.shape(self.image)
+        if self.__class__.images[self.output_image_index] :
+            h, w, bpp = np.shape(self.__class__.images[self.output_image_index])
             # print image properties.
             print('image properties - Width:{0}, Height:{1}'.format(h, w))
 
@@ -91,17 +91,34 @@ class LoadImageFromFile(Step):
         file_path = TextEdit(self)
         file_path.setWindowTitle("Image Filepath")
         file_path.selectionChanged.connect(self.is_valid)
+        file_path.setMaximumHeight(100)
         if self.filepath:
             file_path.setText(self.filepath)
-        # grid layout settings
-        # layout = QGridLayout()
-        layout = QVBoxLayout()
-        layout.addWidget(QLabel('File Path'))
-        layout.addWidget(file_path)
-        input_widget.setLayout(layout)
 
+        #layout settings
+        layout = QFormLayout()
+        layout.addRow(QLabel('File Path'), file_path)
+        input_widget.setLayout(layout)
         return input_widget
 
+    def index_changed(self, index):
+        self.output_image_index = index
+
+    def display_outputs(self):
+        output_widget = QWidget()
+        combo = QComboBox()
+        # populate list of available images
+        for i, image in enumerate(self.images):
+            combo.addItem('Image: {}'.format(i))
+        # connect combo box methods
+        combo.currentIndexChanged.connect(self.index_changed)
+        combo.setCurrentIndex(self.output_image_index)
+        #layout settings
+        layout = QFormLayout()
+        layout.addRow(QLabel('Output Image'), combo)
+        # layout.addWidget(combo)
+        output_widget.setLayout(layout)
+        return output_widget
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)

editor_GUI/step_sequencer/step.py
@@ -1,6 +1,7 @@
 import sys
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel,QGridLayout
+import numpy as np
 
 UNCONFIGURED = 0
 CONFIGURED = 1
@@ -12,6 +13,13 @@ class Step:
     # list to create track of all instantiated objects
     instances = []
     instance_count = 0
+    height = 1920
+    width = 1080
+    display_image = np.ones((height, width), dtype=np.uint8)
+    images = []
+    for i in range(5):
+        images.append(display_image.copy())
+    # images = [display_image.copy() for x in range(5)]
 
     def __init__(self, json=None):
 
@@ -30,7 +38,8 @@ class Step:
                 self.name = json['type'] + str(self.instance_count)
 
         self.status = UNCONFIGURED
-        self.image = None
+        self.input_image_index = 0
+        self.output_image_index = 0
 
     def execute(self, commands, counter):
         pass